29e9ee692659035a5d3b9ac64d8202da2c672d6d
Elye <samhelye@gmail.com>
2023-06-17 10:47:22 +0200

    using directive cleaning

--- Editor/Scripts/Graph/DialogGraphView.cs
+++ Editor/Scripts/Graph/DialogGraphView.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using CheapDialogSystem.Editor.Node;
 using CheapDialogSystem.Runtime.Assets;
 using UnityEditor.Experimental.GraphView;
